551dd6ba9a2f33d5d25ef982810c896b47bcdd85
katenok <a.timin2003@mail.ru>
2021-01-22 21:37:21 +0300

    Added style without color

--- firstLesson/project/js/main.js
+++ firstLesson/project/js/main.js
@@ -1,8 +1,8 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
 ];
 //Функция для формирования верстки каждого товара
 const renderProduct = (title, price) => {
@@ -14,8 +14,20 @@ const renderProduct = (title, price) => {
 };
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title, item.price));
+    // запятые появляются так как метод map трансформирует массив products, по правилу,
+    // указанному в renderProduct(...), тем самым возврящая МАССИВ,
+    // элементы которого (в данном случае - строки разметки продуктов) разделяются запятой, как в любом массиве;
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    // document.querySelector('.products').innerHTML = productsList;
+    // полученный массив (элементы-строки и запятые - разделители элементов) вставляется в .products в качестве текста разметки;
+    // пусть функция getGluedArr(list) выполняет распаковку массива и конкатенацию его элементов-строк в одну строку;
+    const textHTML = getGluedArr(productsList);
+    document.querySelector('.products').innerHTML = textHTML;
+};
+const getGluedArr = list => {
+    let resultString = '';
+    list.forEach((item) => resultString += item);
+    return resultString;
 };
 
 renderPage(products);
