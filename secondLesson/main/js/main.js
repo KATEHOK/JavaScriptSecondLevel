80261fdde0c148ffee5d0db4ddc58134a188130b
katenok <a.timin2003@mail.ru>
2021-01-26 20:38:44 +0300

    Finished main part of HW

--- secondLesson/main/js/main.js
+++ secondLesson/main/js/main.js
@@ -1,38 +1,57 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-// Функция для формирования верстки каждого товара
-// Добавил разметку псевдо-рандомной тематической картинки
-const renderProduct = (item = products[parseInt(Math.random() * products.length)]) => {
-    let randomNum = new Date().getTime();
-    console.log(randomNum);
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <p>${item.pric}</p>
-                <img class="product-img" src='https://loremflickr.com/320/240/${item.title.toLocaleLowerCase()}?random=${randomNum}' alt='${item.title.toLocaleLowerCase()}'>
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+    }
+    getSumPricesOfItems() {
+        const productsList = this.goods;
+        let sum = 0;
+        productsList.forEach(item => sum += item.price);
+        return sum;
+    }
+}
+class ProductItem {
+    constructor(product, img = `img/default.png`) {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+
+    }
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" class="product-img" alt="Some img">
+                <h3 class="product-name">${this.title}</h3>
+                <p class="product-price">${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    // запятые появляются так как метод map трансформирует массив products, по правилу,
-    // указанному в renderProduct(...), тем самым возврящая МАССИВ,
-    // элементы которого (в данном случае - строки разметки продуктов) разделяются запятой, как в любом массиве;
-    console.log(productsList);
-    // document.querySelector('.products').innerHTML = productsList;
+    }
+}
+class Cart {
+    render = () => { };
+    clear = () => { };
+    addProduct = (product) => { };
+    removeProduct = (product) => { };
+}
+class CartItem {
+    render = () => { };
+}
 
-    // полученный массив (элементы-строки и запятые - разделители элементов) вставляется в .products в качестве текста разметки;
-    // пусть функция getGluedArr(list) выполняет распаковку массива и конкатенацию его элементов-строк в одну строку;
-    const textHTML = getGluedArr(productsList);
-    document.querySelector('.products').innerHTML = textHTML;
-};
-const getGluedArr = list => {
-    let resultString = '';
-    list.forEach(item => resultString += item);
-    return resultString;
-};
-
-renderPage(products);
+let list = new ProductsList();
+list.render();
