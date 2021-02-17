b7b48c505ddffac251ab95a54cc94b089e9d840a
katenok <a.timin2003@mail.ru>
2021-02-17 23:10:46 +0300

    Remade main-header

--- eightLesson/personalProject/js/CartComponent.js
+++ eightLesson/personalProject/js/CartComponent.js
@@ -14,59 +14,59 @@ const cart = {
             cartConnection: true,
             showCart: false,
             totalCoast: 0,
-            totalItems: 0
+            totalItems: 0,
         }
     },
     // components: { cartItem },
-    // methods: {
-    //     addProduct(item) {
-    //         this.$root.getJson(`${API}${this.addProductUrl}`)
-    //             .then(data => {
-    //                 if (data.result === 1) {
-    //                     let find = this.cartItems.find(el => el.id_product === item.id_product);
-    //                     if (find) {
-    //                         find.quantity++;
-    //                     } else {
-    //                         const prod = Object.assign({ quantity: 1 }, item);
-    //                         this.cartItems.push(prod)
-    //                     }
-    //                 }
-    //             })
-    //     },
-    //     remove(item) {
-    //         this.$root.getJson(`${API}${this.addProductUrl}`)
-    //             .then(data => {
-    //                 if (data.result === 1) {
-    //                     if (item.quantity > 1) {
-    //                         item.quantity--;
-    //                     } else {
-    //                         this.cartItems.splice(this.cartItems.indexOf(item), 1);
-    //                     }
-    //                 }
+    methods: {
+        //     addProduct(item) {
+        //         this.$root.getJson(`${API}${this.addProductUrl}`)
+        //             .then(data => {
+        //                 if (data.result === 1) {
+        //                     let find = this.cartItems.find(el => el.id_product === item.id_product);
+        //                     if (find) {
+        //                         find.quantity++;
+        //                     } else {
+        //                         const prod = Object.assign({ quantity: 1 }, item);
+        //                         this.cartItems.push(prod)
+        //                     }
+        //                 }
+        //             })
+        //     },
+        //     remove(item) {
+        //         this.$root.getJson(`${API}${this.addProductUrl}`)
+        //             .then(data => {
+        //                 if (data.result === 1) {
+        //                     if (item.quantity > 1) {
+        //                         item.quantity--;
+        //                     } else {
+        //                         this.cartItems.splice(this.cartItems.indexOf(item), 1);
+        //                     }
+        //                 }
 
-    //             })
-    //     },
-    // },
-    // mounted() {
-    //     this.$root.getJson(`${API}${this.$data.cartUrl}`)
-    //         .then(data => {
-    //             // console.dir(data);
-    //             for (let item of data.contents) {
-    //                 // console.log(item);
-    //                 item.img = `img/id_product-${item.id_product}.png`;
-    //                 this.$data.cartItems.push(item);
-    //                 this.$data.cartConnection = true;
-    //             }
-    //         })
-    //         .catch(() => this.$data.cartConnection = false);
-    // },
+        //             })
+        //     },
+        // },
+        // mounted() {
+        //     this.$root.getJson(`${API}${this.$data.cartUrl}`)
+        //         .then(data => {
+        //             // console.dir(data);
+        //             for (let item of data.contents) {
+        //                 // console.log(item);
+        //                 item.img = `img/id_product-${item.id_product}.png`;
+        //                 this.$data.cartItems.push(item);
+        //                 this.$data.cartConnection = true;
+        //             }
+        //         })
+        //         .catch(() => this.$data.cartConnection = false);
+    },
     template: `
         <div class="cart" v-show="$data.showCart">
             <div class="cart-controlers">
                 <span class="cart-controlers-sum cart-controlers-item">Total coast:&nbsp;\${{ $data.totalCoast }}</span>
                 <span class="cart-controlers-quantity cart-controlers-item">Total items:&nbsp;{{ $data.totalItems }}</span>
                 <button class="cart-controlers-pay-btn cart-controlers-item">Pay</button>
-                <button class="cart-controlers-close-btn cart-controlers-item" @click="$data.showCart = false">&times;</button>
+                <button class="cart-controlers-close-btn cart-controlers-item" @click="$data.showCart = !$data.showCart">&times;</button>
             </div>        
         </div>
     `
